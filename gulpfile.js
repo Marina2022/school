4910ba0f9a6172aad4e79bb44753d752d104747a
Marina2022 <marusiiiia@yandex.ru>
2023-02-22 12:29:04 +0300

    do modal-auth desktop

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ const copyImg = () => {
 const makeWebp = () => {
   return gulp
     .src("source/img/**/*.{jpg,png}")
-    .pipe(webp({ quality: 90 }))
+    //.pipe(webp({ quality: 90 }))
     .pipe(gulp.dest("build/img"));
 };
 
@@ -115,7 +115,7 @@ export default gulp.series(
   html,
   scripts,
   copyImg,
-  makeWebp,
+  //makeWebp,
   server,
   watcher
 );
--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,21 @@
-const ps = new PerfectScrollbar('.content-block', {
-  wheelSpeed: 2,
-  wheelPropagation: true,
-  minScrollbarLength: 100,
+if (document.querySelector('.content-block')) {  // чтобы не вылетал в ошибку, если нет блока
+  const ps = new PerfectScrollbar('.content-block', {
+    wheelSpeed: 2,
+    wheelPropagation: true,
+    minScrollbarLength: 100,
+  });
+}
 
+const loginBtn = document.querySelector('.auth-modal__login-btn');
 
-});
+const loginErrorMessage = document.querySelector('.login-error-message');
+console.log(loginErrorMessage);
+console.log('привет');
+
+loginBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  alert('ha');
+  loginErrorMessage.classList.toggle('show');
+
+})
 
