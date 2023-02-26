620b6fd4afa84e28ee65d39671b7b52737365b99
Marina2022 <marusiiiia@yandex.ru>
2023-02-26 14:10:50 +0300

    open-close modals script

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,49 @@
-if (document.querySelector('.content-block')) {  // чтобы не вылетал в ошибку, если нет блока
-  const ps = new PerfectScrollbar('.content-block', {
-    wheelSpeed: 2,
-    wheelPropagation: true,
-    minScrollbarLength: 100,
-  });
-  if (document.querySelector('.content-block--no-scroll')) {  //
+const modal = document.querySelector('.auth-modal');
+const lkBtn = document.querySelector('.user_link');
+const page = document.querySelector('.page');
+
+let ps;
+
+const scrollInit = () => {
+  if (document.querySelector('.content-block')) {  // чтобы не вылетал в ошибку, если нет блока
+    ps = new PerfectScrollbar('.content-block', {
+      wheelSpeed: 2,
+      wheelPropagation: true,
+      minScrollbarLength: 100,
+    });
+  }
+  if(!modal.classList.contains('hidden')) ps.destroy();
+}
+scrollInit();
+
+// открытие/закрытие модалок
+
+lkBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const escHandler = (e) => {
+    console.log(e.keyCode);
+    if (e.keyCode === 27) modal.classList.add('hidden');
+    page.removeEventListener('keydown', escHandler);
+    scrollInit();
+  }
+
+  modal.classList.remove('hidden');
+  const closeModalBtn = document.querySelector('.modal-close-btn');
+
+  closeModalBtn.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    scrollInit();
+  })
+  page.addEventListener('keydown', escHandler);
+
+  if (document.querySelector('.content-block')) { // т.е. для всех страниц, кроме index.html
     ps.destroy();
   }
-}
+})
+
+
+// Появление надписи "неправильный пароль или имейл" по кнопке Вход
 
 const loginBtn = document.querySelector('.auth-modal__login-btn');
 const loginErrorMessage = document.querySelector('.login-error-message');
@@ -21,7 +57,7 @@ if (loginBtn) {
 
 // скрипт убирает звезду, когда в инпуте появляется текст (и возвращает обратно, когда текст убирается)
 const inputWithStarsAll = document.querySelectorAll('.feedback-input--with-star');
-inputWithStarsAll.forEach((input)=>{
+inputWithStarsAll.forEach((input) => {
   const starInputLabel = input.closest('.form-placeholder-wrapper').querySelector('.star-input-label');
   input.addEventListener('input', (e) => {
     if (e.target.value !== '') starInputLabel.classList.add('hidden');
